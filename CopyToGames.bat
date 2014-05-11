deb42cefa7e111beb946ae00bf9c979fba37a863
bo3b <github@bo3b.net>
2014-05-10 21:14:02 -0700

    Update the convenience CopyToGames.bat to only work for user.
    It now checks %username% == bo3b, and if not, exits without copy, so it won't impact others, but can still be checked in.

--- CopyToGames.bat
+++ CopyToGames.bat
@@ -1,5 +1,10 @@
 @echo off
-REM echo 
-REM xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
-REM echo 
-REM xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\SteamLibrary\SteamApps\common\BioShock Infinite\Binaries\Win32\" /F /Y
+if "%username%" NEQ "Bo3b" exit
+
+@echo on
+echo 
+xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
+echo 
+xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\Bootleg\Assassin's Creed IV - Black Flag\"  /F /Y
+echo 
+xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\SteamLibrary\SteamApps\common\BioShock Infinite\Binaries\Win32\" /F /Y
