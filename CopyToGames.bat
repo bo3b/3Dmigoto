9605c6798632b83062bb734106fbe5e49605c9c7
bo3b <github@bo3b.net>
2014-05-14 03:18:30 -0700

    Straighten out build process a bit, convenience copy for debug and release.

--- CopyToGames.bat
+++ CopyToGames.bat
@@ -3,8 +3,10 @@ if "%username%" NEQ "Bo3b" exit
 
 @echo on
 echo 
-xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
+xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
 echo 
-xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\Bootleg\Assassin's Creed IV - Black Flag\"  /F /Y
+xcopy "%1*.*" "T:\Bootleg\Assassin's Creed IV - Black Flag\"  /F /Y
 echo 
-xcopy "C:\Users\Bo3b\Documents\Code\3DMigoto\Debug\*.*" "T:\SteamLibrary\SteamApps\common\BioShock Infinite\Binaries\Win32\" /F /Y
+xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\BioShock Infinite\Binaries\Win32\" /F /Y
+echo 
+xcopy "%1*.*" "T:\Bootleg\Saints Row IV\" /F /Y
