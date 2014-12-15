2817bfcabb9a945ad47e07d87d743d37c3ea12f7
bo3b <github@bo3b.net>
2014-12-14 16:24:35 -0800

    First pass at making a standalone Decompiler.
    Not ready to use, but basic structure seems good.

---
+++ HLSLDecompiler/cmd_Decompiler/ReadMe.txt
@@ -0,0 +1,44 @@
+========================================================================
+    CONSOLE APPLICATION : cmd_Decompiler Project Overview
+========================================================================
+
+AppWizard has created this cmd_Decompiler application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your cmd_Decompiler application.
+
+
+cmd_Decompiler.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+cmd_Decompiler.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+cmd_Decompiler.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named cmd_Decompiler.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
+
+Created as a way to access the Decompiler via the Cmd window/console.  
+This will allow us to test and Debug the Decompiler without needing anything except an ASM file,
+and will also allows us to create some unit tests of previously known good shaders.
---
+++ HLSLDecompiler/cmd_Decompiler/cmd_Decompiler.cpp
@@ -0,0 +1,85 @@
+// cmd_Decompiler.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>     // console output 
+
+#include "..\DecompileHLSL.h"
+
+//----------------------------------------------------------------------------- 
+// Print help lines on errors. 
+//----------------------------------------------------------------------------- 
+void PrintHelp()
+{
+	using std::cout;
+	using std::endl;
+
+	cout << endl;
+	cout << L"3Dmigoto standalone Decompiler..." << endl;
+	cout << endl;
+	cout << L"Specify the ASM file name to Decompile, as command line parameter." << endl;
+	cout << endl;
+}
+
+
+std::string Decompile(_TCHAR* asmFileName)
+{
+	using std::cout;
+	using std::endl;
+	using std::string;
+
+	cout << "    creating HLSL representation.\n" << endl;
+
+	bool patched = false;
+	string shaderModel;
+	bool errorOccurred = false;
+
+	// Set all to zero, so we only init the ones we are using here.
+	ParseParameters p = {};
+
+	p.bytecode = pShaderByteCode->GetBufferPointer();
+	p.decompiled = (const char *)disassembly->GetBufferPointer();
+	p.decompiledSize = disassembly->GetBufferSize();
+	const string decompiledCode = DecompileBinaryHLSL(p, patched, shaderModel, errorOccurred);
+
+	if (!decompiledCode.size())
+	{
+		if (LogFile) fprintf(LogFile, "    error while decompiling.\n");
+	}
+
+	return decompiledCode;
+}
+
+
+//----------------------------------------------------------------------------- 
+// Console App Entry-Point. 
+//----------------------------------------------------------------------------- 
+int _tmain(int argc, _TCHAR* argv[])
+{
+	static const int kExitOk = 0;
+	static const int kExitError = 1;
+
+	try
+	{
+		// Check command line arguments 
+		if (argc != 2)
+		{
+			PrintHelp();
+		}
+		else
+		{
+			// Print file hash 
+			std::string hlsl = Decompile(argv[1]);
+			std::cout << hlsl << std::endl;
+		}
+	}
+	catch (const std::exception & e)
+	{
+		std::cerr << "\n*** ERROR: " << e.what() << std::endl;
+		return kExitError;
+	}
+
+	return kExitOk;
+}
+
