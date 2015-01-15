1fc4454c321716fc7e36d05092823bb3abe5b48f
Ian Munsie <darkstarsword@gmail.com>
2015-01-15 23:01:19 +1100

    Clear current depth buffer on OMSetRenderTargets
    Without clearing this we would attribute the last used depth target to a
    shader that is used without any depth target.

--- DirectX11/Direct3D11Context.h
+++ DirectX11/Direct3D11Context.h
@@ -1204,6 +1204,7 @@ STDMETHODIMP_(void) D3D11Wrapper::ID3D11DeviceContext::OMSetRenderTargets(THIS_
 	{
 		if (G->ENABLE_CRITICAL_SECTION) EnterCriticalSection(&G->mCriticalSection);
 		G->mCurrentRenderTargets.clear();
+		G->mCurrentDepthTarget = NULL;
 		if (G->ENABLE_CRITICAL_SECTION) LeaveCriticalSection(&G->mCriticalSection);
 
 		for (UINT i = 0; i < NumViews; ++i)
