a7e6a515ce4460cdabc6ac7cd324fb5eb443f181
bo3b <github@bo3b.net>
2015-02-15 17:50:22 -0800

    Correct the .RC files for DirectX11 and NVAPI projects for real versions.
    This updates the .RC file for DirectX11, and adds a new RC file for NVAPI to use the correct version from the updated version.h file.
    
    This will now apply a version number to both DLLs, that matches the project version and is viewable via properties.
    
    This removes the wrong and misleading microsoft version and file info, and replaces it with correct info.   Built into the dll files now, yeah!
    
    Tested with Mordor to be sure DLLs still load, and they do.
    
    To use this, all we need to do is update version.h to new numbers.  Next step will be to automate the Publish to auto-increment.

---
+++ version.h
@@ -0,0 +1,46 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DirectX11.rc
+// Used by NVAPI.rc
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+// Actual version, name, copyright information that is built into the dlls.
+// This is what an end-user will see when they look at dll properties.
+
+#define VERSION_MAJOR               0
+#define VERSION_MINOR               99
+#define VERSION_REVISION            42
+ 
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+
+#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
+#define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
+                                    "." STRINGIZE(VERSION_MINOR)    \
+                                    "." STRINGIZE(VERSION_REVISION) \
+ 
+#define VER_PRODUCTNAME_STR         "3Dmigoto"
+#define VER_PRODUCT_VERSION         VER_FILE_VERSION
+#define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
+#define VER_COPYRIGHT_STR           "Copyright (C) 2014-2015"
+ 
+#ifdef _DEBUG
+  #define VER_VER_DEBUG             VS_FF_DEBUG
+#else
+  #define VER_VER_DEBUG             0
+#endif
+ 
+#define VER_FILEOS                  VOS_NT_WINDOWS32
+#define VER_FILEFLAGS               VER_VER_DEBUG
+#define VER_FILETYPE                VFT_APP
+
