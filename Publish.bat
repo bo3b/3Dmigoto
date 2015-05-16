93434a8085e3448f14c58a8c6b21c48a3d54affe
bo3b <github@bo3b.net>
2015-05-15 22:05:45 -0700

    Add the courierbold.spritefont to Zip releases.
    Needed for the overlay to show anything, at present it's an extra file that needs to tag along.

--- Publish.bat
+++ Publish.bat
@@ -126,12 +126,14 @@ MKDIR ".\Zip Release\x32\"
 MOVE ".\Zip Release\d3dx.ini"  ".\Zip Release\x32\"
 MOVE ".\Zip Release\uninstall.bat"  ".\Zip Release\x32\"
 MOVE ".\Zip Release\*.dll"  ".\Zip Release\x32\"
+MOVE ".\Zip Release\courierbold.spritefont"  ".\Zip Release\x32\"
 
 echo(
 MKDIR ".\Zip Release\x64\"
 MOVE ".\x64\Zip Release\d3dx.ini"  ".\Zip Release\x64\"
 MOVE ".\x64\Zip Release\uninstall.bat"  ".\Zip Release\x64\"
 MOVE ".\x64\Zip Release\*.dll"  ".\Zip Release\x64\"
+MOVE ".\x64\Zip Release\courierbold.spritefont"  ".\Zip Release\x64\"
 
 echo(
 echo(
