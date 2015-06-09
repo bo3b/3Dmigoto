801629544ecd1eba40844fdb3bb08d6c4d07ee4a
Ian Munsie <darkstarsword@gmail.com>
2015-06-09 11:18:47 +1000

    Incremental Publish build: 1.1.17

--- version.h
+++ version.h
@@ -21,7 +21,7 @@
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               1
-#define VERSION_REVISION            16
+#define VERSION_REVISION            17
  
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
