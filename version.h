dbf8b6ecfa7f97319a7afddb0d7728b5a6842819
Ian Munsie <darkstarsword@gmail.com>
2015-06-27 23:33:32 +1000

    Revert "Revert "Incremental Publish build: 1.1.23""
    Not entirely sure why this was reverted, but we don't want our version
    numbers being repeated or going backwards, so revert the revert.
    
    This reverts commit dfe28b2b01acd34c49f39dee95d837fb0c5c0be2.

--- version.h
+++ version.h
@@ -21,7 +21,7 @@
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               1
-#define VERSION_REVISION            22
+#define VERSION_REVISION            23
  
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
