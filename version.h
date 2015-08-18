9a6dfcb0c9102abbc3c7b3e4ff8733e9c23257fe
bo3b <github@bo3b.net>
2015-08-18 01:28:59 -0700

    Incremental Publish build: 1.2.1

--- version.h
+++ version.h
@@ -26,7 +26,7 @@
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               2
-#define VERSION_REVISION            0
+#define VERSION_REVISION            1
  
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
