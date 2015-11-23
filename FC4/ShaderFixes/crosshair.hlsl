4f96a277df903c8d07aff296809cf963ec124890
Ian Munsie <darkstarsword@gmail.com>
2015-11-23 20:42:41 +1100

    Far Cry 4: Use stereo2mono to improve auto HUD depth
    With the new ability to copy both left and right sides of a resource
    into a shader, we can improve the auto HUD depth. The original code
    calculated the adjustment amount independently in each eye, which worked
    surprisingly well, but was not perfect in situations where an object in
    the foreground intercepted the ray in just one eye, which could cause
    the crosshair to shift in one eye.
    
    Now with the ability to check the depth buffer from both eyes
    simultaneously we can change the crosshair code so that the rays from
    both eyes need to intersect an object simultaneously to treat it as a
    hit and move the crosshair over the object.

--- FC4/ShaderFixes/crosshair.hlsl
+++ FC4/ShaderFixes/crosshair.hlsl
@@ -75,3 +75,80 @@ float adjust_from_depth_buffer(float x, float y)
 
 	return old_offset;
 }
+
+float adjust_from_stereo2mono_depth_buffer(float x, float y)
+{
+	float4 stereo = StereoParams.Load(0);
+	float separation = stereo.x; float convergence = stereo.y;
+	float old_offset, offset, w, left, right, left_sampled_w, right_sampled_w, sampled_w, distance;
+	uint i;
+
+	// Stereo cursor: To improve the accuracy of the stereo cursor, we
+	// sample a number of points on the depth buffer, starting at the near
+	// clipping plane and working towards original x + separation.
+	//
+	// You can think of this as a line in three dimensional space that
+	// starts at each eye and stretches out towards infinity. We sample 255
+	// points along this line (evenly spaced in the X axis) and compare
+	// with the depth buffer to find where the line is first intersected.
+	//
+	// This particular variant uses the depth information from both eyes
+	// (using 3DMigoto's stereo2mono feature) so we can find where the
+	// lines from both eyes are intersected simultaneously for a more
+	// accurate adjustment.
+	//
+	// Note: The reason for sampling 255 points came from a restriction in
+	// DX9/SM3 where loops had to run a constant number of iterations and
+	// there was no way to set that number from within the shader itself.
+	// I'm not sure if the same restriction applies in DX11 with SM4/5 - if
+	// it doesn't, we could change this to check each pixel instead for
+	// better accuracy.
+	//
+	// Based on DarkStarSword's stereo crosshair code originally developed
+	// for Miasmata, adapted to Unity, translated to HLSL, then updated to
+	// make use of 3DMigoto's stereo2mono feature.
+
+	float asep = abs(separation);
+
+	offset = -convergence * asep;	// Z = X offset from center
+	distance = asep - offset;	// Total distance to cover (asep - starting X offset)
+
+	old_offset = offset;
+	for (i = 0; i < 255; i++) {
+		offset += distance / 255.0;
+
+		// Calculate depth for this point on the line:
+		w = (asep * convergence) / (asep - offset);
+
+		float left = max((x - offset) / 2 + 0.5, 0);
+		float right = min((x + offset) / 2 - 0.5, 0);
+
+		left_sampled_w = world_z_from_depth_buffer(left, y);
+		right_sampled_w = world_z_from_depth_buffer(right, y);
+
+		// Only adjust the crosshair once it has intersected something in *both* eyes:
+		sampled_w = max(left_sampled_w, right_sampled_w);
+
+		// Adjust crosshair in both eyes once it intersects something in *either* eye:
+		//sampled_w = min(left_sampled_w, right_sampled_w);
+
+		// Equivalent to normal auto depth adjustment for a regular stereo depth buffer:
+		//sampled_w = world_z_from_depth_buffer(x + offset * -stereo.z, y);
+
+		// Equivalent to normal auto depth adjustment for a 2x width mono depth buffer:
+		//sampled_w = world_z_from_depth_buffer((x + offset * -stereo.z) / 2 - 0.5 * -stereo.z, y);
+
+		if (sampled_w == 0)
+			return 0;
+
+		// If the sampled depth is closer than the calculated depth,
+		// we have found something that intersects the line, so exit
+		// the loop and return the last point that was not intersected:
+		if (w > sampled_w)
+			break;
+
+		old_offset = offset;
+	}
+
+	return old_offset * -stereo.z;
+}
