30f6f9c5c35a8f34ff12760ff43ddfeb508a53ee
bo3b <github@bo3b.net>
2015-12-01 01:14:11 -0800

    Add a bunch more uuidof conversions in util.h.
    No point in having strings of hex in the logs, so this pass is to include *every* interesting object that might be referenced by a UUID.  This includes everything from dxgi.h, dxgi1_2.h, and d3d11_1.h.
    
    Since we are building against 8.0 libraries, this is the complete set.

--- util.h
+++ util.h
@@ -414,28 +414,62 @@ static DXGI_FORMAT EnsureNotTypeless( DXGI_FORMAT fmt )
 // the actual names of the objects in question.
 // e.g.
 // DEFINE_GUID(IID_IDXGIFactory,0x7b7166ec,0x21c7,0x44ae,0xb2,0x1a,0xc9,0xae,0x32,0x1a,0xe3,0x69);
+// 
 
 static std::string NameFromIID(IID id)
 {
+	// Adding every MIDL_INTERFACE from d3d11_1.h to make this reporting complete.
+	// Doesn't seem useful to do every object from d3d11.h itself.
+
 	if (__uuidof(IUnknown) == id)
 		return "IUnknown";
 
-	if (__uuidof(ID3D11Device) == id)
-		return "ID3D11Device";
-	if (__uuidof(ID3D10Device) == id)
-		return "ID3D10Device";
+	if (__uuidof(ID3D11DeviceChild) == id)
+		return "ID3D11DeviceChild";
+	if (__uuidof(ID3DDeviceContextState) == id)
+		return "ID3DDeviceContextState";
+
 	if (__uuidof(IDirect3DDevice9) == id)
 		return "IDirect3DDevice9";
-
+	if (__uuidof(ID3D10Device) == id)
+		return "ID3D10Device";
+	if (__uuidof(ID3D11Device) == id)
+		return "ID3D11Device";
 	if (__uuidof(ID3D11Device1) == id)
 		return "ID3D11Device1";
+	//if (__uuidof(ID3D11Device2) == id)  for d3d11_2.h when the time comes
+	//	return "ID3D11Device2";
+
+	if (__uuidof(ID3D11DeviceContext) == id)
+		return "ID3D11DeviceContext";
 	if (__uuidof(ID3D11DeviceContext1) == id)
 		return "ID3D11DeviceContext1";
-	
-	if (__uuidof(ID3D11InfoQueue) == id)
-		return "ID3D11InfoQueue ";
+	//if (__uuidof(ID3D11DeviceContext2) == id) for d3d11_2.h when the time comes
+	//	return "ID3D11DeviceContext2";
 
-	// All the DXGI interfaces
+	if (__uuidof(ID3D11InfoQueue) == id)
+		return "ID3D11InfoQueue";
+	if (__uuidof(ID3DUserDefinedAnnotation) == id)
+		return "ID3DUserDefinedAnnotation";
+
+	if (__uuidof(ID3D11BlendState) == id)
+		return "ID3D11BlendState";
+	if (__uuidof(ID3D11BlendState1) == id)
+		return "ID3D11BlendState1";
+	if (__uuidof(ID3D11RasterizerState) == id)
+		return "ID3D11RasterizerState";
+	if (__uuidof(ID3D11RasterizerState1) == id)
+		return "ID3D11RasterizerState1";
+
+	if (__uuidof(ID3D11Texture2D) == id)	// Used to fetch backbuffer
+		return "ID3D11Texture2D";
+
+	// All the DXGI interfaces from dxgi.h, and dxgi1_2.h
+
+	if (__uuidof(IDXGIObject) == id)
+		return "IDXGIObject";
+	if (__uuidof(IDXGIDeviceSubObject) == id)
+		return "IDXGIDeviceSubObject";
 
 	if (__uuidof(IDXGIFactory) == id)
 		return "IDXGIFactory";
@@ -463,11 +497,27 @@ static std::string NameFromIID(IID id)
 	if (__uuidof(IDXGIAdapter2) == id)
 		return "IDXGIAdapter2";
 
-	if (__uuidof(IDXGIObject) == id)
-		return "IDXGIObject";
+	if (__uuidof(IDXGIOutputDuplication) == id)
+		return "IDXGIOutputDuplication";
+	if (__uuidof(IDXGIDisplayControl) == id)
+		return "IDXGIDisplayControl";
 
-	if (__uuidof(ID3D11Texture2D) == id)	// Used to fetch backbuffer
-		return "ID3D11Texture2D";
+	if (__uuidof(IDXGIOutput) == id)
+		return "IDXGIOutput";
+	if (__uuidof(IDXGIOutput1) == id)
+		return "IDXGIOutput1";
+	if (__uuidof(IDXGIResource) == id)
+		return "IDXGIResource";
+	if (__uuidof(IDXGIResource1) == id)
+		return "IDXGIResource1";
+	if (__uuidof(IDXGISurface) == id)
+		return "IDXGISurface";
+	if (__uuidof(IDXGISurface1) == id)
+		return "IDXGIResource";
+	if (__uuidof(IDXGISurface2) == id)
+		return "IDXGISurface2";
+	if (__uuidof(IDXGIKeyedMutex) == id)
+		return "IDXGIKeyedMutex";
 
 	// For unknown IIDs lets return the hex string.
 	// Converting from wchar_t to string using stackoverflow suggestion.
