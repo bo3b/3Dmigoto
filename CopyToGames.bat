4d30e2b9aac8ef45fb6810ae66958c682e7c0d53
bo3b <github@bo3b.net>
2016-03-20 19:12:50 -0700

    Add starting broken shader for hand edit.
    Also update convenience copy for JC3.

--- CopyToGames.bat
+++ CopyToGames.bat
@@ -6,15 +6,15 @@ if %2=="amd64" GOTO Copyx64
 :Copyx86
 @echo on
 
-xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
-xcopy "%1*.*" "T:\Bootleg\Assassin's Creed IV - Black Flag\"  /F /Y
-xcopy "%1*.*" "T:\Bootleg\Saints Row IV\" /F /Y
-xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\Saints Row the Third\" /F /Y
-xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\F.E.A.R. 3\" /F /Y
+xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
+xcopy /C "%1*.*" "T:\Bootleg\Assassin's Creed IV - Black Flag\"  /F /Y
+xcopy /C "%1*.*" "T:\Bootleg\Saints Row IV\" /F /Y
+xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\Saints Row the Third\" /F /Y
+xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\F.E.A.R. 3\" /F /Y
 xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\Alien Isolation\" /F /Y
-xcopy "%1*.*" "T:\Bootleg\Ori and the Blind Forest\" /F /Y
-xcopy "%1*.*" "C:\Program Files (x86)\Steam\SteamApps\common\DefenseGrid2\" /F /Y
-xcopy "%1*.*" "T:\Bootleg\DiRT Rally\" /F /Y
+xcopy /C "%1*.*" "T:\Bootleg\Ori and the Blind Forest\" /F /Y
+xcopy /C "%1*.*" "C:\Program Files (x86)\Steam\SteamApps\common\DefenseGrid2\" /F /Y
+xcopy /C "%1*.*" "T:\Bootleg\DiRT Rally\" /F /Y
 
 exit
 
@@ -22,10 +22,11 @@ exit
 :Copyx64
 @echo on
 echo 
-xcopy "%1*.*" "T:\Games\Watch_Dogs\bin"  /F /Y
-xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\Call of Duty Ghosts\"  /F /Y
-xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\ShadowOfMordor\x64\" /F /Y
-xcopy "%1*.*" "T:\Bootleg\Project CARS\" /F /Y
+xcopy /C "%1*.*" "T:\Games\Watch_Dogs\bin"  /F /Y
+xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\Call of Duty Ghosts\"  /F /Y
+xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\ShadowOfMordor\x64\" /F /Y
+xcopy /C "%1*.*" "T:\Bootleg\Project CARS\" /F /Y
 xcopy /C "%1*.*" "T:\Games\The Witcher 3 Wild Hunt\bin\x64\" /F /Y
-xcopy "%1*.*" "T:\SteamLibrary\SteamApps\common\Metal Gear Solid Ground Zeroes\" /F /Y
-xcopy "%1*.*" "T:\Games\Far Cry 4\bin\" /F /Y
+xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\Metal Gear Solid Ground Zeroes\" /F /Y
+xcopy /C "%1*.*" "T:\Games\Far Cry 4\bin\" /F /Y
+xcopy /C "%1*.*" "T:\SteamLibrary\SteamApps\common\Just Cause 3\" /F /Y
