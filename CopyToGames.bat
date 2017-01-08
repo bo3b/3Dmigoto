6f0fa2a865e1f2484ad9e7c03c6d7b7e271a07c4
bo3b <git@bo3b.net>
2017-01-07 21:12:37 -0800

    Update convenience copy for platform update testing.

--- CopyToGames.bat
+++ CopyToGames.bat
@@ -7,14 +7,12 @@ if %2=="amd64" GOTO Copyx64
 @echo on
 
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
-xcopy /C "%1*.*" "W:\Bootleg\Assassin's Creed IV - Black Flag\"  /F /Y
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row IV\" /F /Y
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row the Third\" /F /Y
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\F.E.A.R. 3\" /F /Y
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Alien Isolation\" /F /Y
-xcopy /C "%1*.*" "W:\Bootleg\Ori and the Blind Forest\" /F /Y
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DefenseGrid2\" /F /Y
-xcopy /C "%1*.*" "W:\Bootleg\DiRT Rally\" /F /Y
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DiRT Rally\" /F /Y
 
 exit
 
@@ -31,3 +29,5 @@ xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Metal Gear Solid Ground Zeroe
 xcopy /C "%1*.*" "W:\Games\Far Cry 4\bin\" /F /Y
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Just Cause 3\" /F /Y
 xcopy /C "%1*.*" "W:\Games\The Crew (Worldwide)\" /F /Y
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Batman The Telltale Series\" /F /Y
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Dishonored2\" /F /Y
