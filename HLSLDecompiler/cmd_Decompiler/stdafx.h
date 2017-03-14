face481cbc496d868bd1a86c54010e4687566159
ChenA <chena.invalid@gmail.com>
2017-03-14 15:04:08 +0800

    fix compile error

--- HLSLDecompiler/cmd_Decompiler/stdafx.h
+++ HLSLDecompiler/cmd_Decompiler/stdafx.h
@@ -20,25 +20,25 @@
 
 using namespace std;
 
-struct shader_ins
-{
-	unsigned opcode : 11;
-	unsigned _11_23 : 13;
-	unsigned length : 7;
-	unsigned extended : 1;
-};
-struct token_operand
-{
-	unsigned comps_enum : 2; /* sm4_operands_comps */
-	unsigned mode : 2; /* sm4_operand_mode */
-	unsigned sel : 8;
-	unsigned file : 8; /* SM_FILE */
-	unsigned num_indices : 2;
-	unsigned index0_repr : 3; /* sm4_operand_index_repr */
-	unsigned index1_repr : 3; /* sm4_operand_index_repr */
-	unsigned index2_repr : 3; /* sm4_operand_index_repr */
-	unsigned extended : 1;
-};
+//struct shader_ins
+//{
+//	unsigned opcode : 11;
+//	unsigned _11_23 : 13;
+//	unsigned length : 7;
+//	unsigned extended : 1;
+//};
+//struct token_operand
+//{
+//	unsigned comps_enum : 2; /* sm4_operands_comps */
+//	unsigned mode : 2; /* sm4_operand_mode */
+//	unsigned sel : 8;
+//	unsigned file : 8; /* SM_FILE */
+//	unsigned num_indices : 2;
+//	unsigned index0_repr : 3; /* sm4_operand_index_repr */
+//	unsigned index1_repr : 3; /* sm4_operand_index_repr */
+//	unsigned index2_repr : 3; /* sm4_operand_index_repr */
+//	unsigned extended : 1;
+//};
 
 vector<DWORD> assembleIns(string s);
 vector<byte> readFile(string fileName);
