54adbf0797b3ef717fd24d64742c335feb7e4dbf
bo3b <3dmigoto@bo3b.net>
2017-03-24 01:37:02 -0700

    Update convenience copytogames for /S
    This copies the directory structure too, which includes the sbs shaders into ShaderFixes.  This fixes errors at launch when it's missing and thus out of sync with d3dx.ini.
    
    Also add HeadLander to the testing list, as it crashes very similarly to Dishonored2.

--- CopyToGames.bat
+++ CopyToGames.bat
@@ -6,13 +6,13 @@ if %2=="amd64" GOTO Copyx64
 :Copyx86
 @echo on
 
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row IV\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row the Third\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\F.E.A.R. 3\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Alien Isolation\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DefenseGrid2\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DiRT Rally\" /F /Y
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Assassin's Creed 3\"  /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row IV\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row the Third\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\F.E.A.R. 3\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Alien Isolation\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DefenseGrid2\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DiRT Rally\" /F /Y /S
 
 exit
 
@@ -20,14 +20,15 @@ exit
 :Copyx64
 @echo on
 echo 
-xcopy /C "%1*.*" "W:\Games\Watch_Dogs\bin"  /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Call of Duty Ghosts\"  /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\ShadowOfMordor\x64\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Project CARS\" /F /Y
-xcopy /C "%1*.*" "W:\Games\The Witcher 3 Wild Hunt\bin\x64\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Metal Gear Solid Ground Zeroes\" /F /Y
-xcopy /C "%1*.*" "W:\Games\Far Cry 4\bin\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Just Cause 3\" /F /Y
-xcopy /C "%1*.*" "W:\Games\The Crew (Worldwide)\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Batman The Telltale Series\" /F /Y
-xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Dishonored2\" /F /Y
+xcopy /C "%1*.*" "W:\Games\Watch_Dogs\bin"  /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Call of Duty Ghosts\"  /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\ShadowOfMordor\x64\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Project CARS\" /F /Y /S
+xcopy /C "%1*.*" "W:\Games\The Witcher 3 Wild Hunt\bin\x64\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Metal Gear Solid Ground Zeroes\" /F /Y /S
+xcopy /C "%1*.*" "W:\Games\Far Cry 4\bin\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Just Cause 3\" /F /Y /S
+xcopy /C "%1*.*" "W:\Games\The Crew (Worldwide)\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Batman The Telltale Series\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Dishonored2\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\HeadLander\" /F /Y /S
