d135604498bfef987f37351d739d11de096958e4
bo3b <3dmigoto@bo3b.net>
2017-04-04 17:06:05 -0700

    Return HackerContext1 for QueryInterface.
    If it's already the right wrap, we return that object.  If it's not, we create a new wrapped object, including the HackerDevice1 object that is required.
    
    This is similar to the sequence used for CreateDeviceAndSwapChain.

--- DirectX11/HackerContext.cpp
+++ DirectX11/HackerContext.cpp
@@ -648,13 +648,17 @@ HRESULT STDMETHODCALLTYPE HackerContext::QueryInterface(
 	{
 		if (!G->enable_platform_update) 
 		{
-			LogInfo("  returns E_NOINTERFACE as error for ID3D11DeviceContext1 (try allow_platform_update=1 if the game refuses to run). \n");
+			LogInfo("***  returns E_NOINTERFACE as error for ID3D11DeviceContext1 (try allow_platform_update=1 if the game refuses to run). \n");
 			*ppvObject = NULL;
 			return E_NOINTERFACE;
 		}
 
-		*ppvObject = this;
-		LogDebug("  return HackerContext1(%s@%p) wrapper of %p \n", type_name(this), this, mOrigContext);
+		// If this happens to already be a HackerContext1, we don't want to rewrap it or make a new one.
+		if (dynamic_cast<HackerContext1*>(this) != NULL)
+		{
+			*ppvObject = this;
+			LogDebug("  return HackerContext1(%s@%p) wrapper of %p \n", type_name(this), this, ppvObject);
+		}
 
 		// For Batman: TellTale games, they call this to fetch the DeviceContext1.
 		// We need to return a hooked version as part of fleshing it out for games like this
@@ -662,27 +666,31 @@ HRESULT STDMETHODCALLTYPE HackerContext::QueryInterface(
 		// Not at all positive this is the right approach, the mix of type1 objects and
 		// Hacker objects is a bit obscure.
 
-		//ID3D11DeviceContext1 *origDeviceContext1;
-		//ID3D11Device *origDevice;
-		//ID3D11Device1 *origDevice1;
-		//origDeviceContext1 = static_cast<ID3D11DeviceContext1*>(*ppvObject);
-		//origDeviceContext1->GetDevice(&origDevice);
-		//HRESULT hr = origDevice->QueryInterface(IID_PPV_ARGS(&origDevice1));
-		//if (FAILED(hr))
-		//{
-		//	LogInfo("  failed QueryInterface of ID3D11Device1 = %x for %p \n", hr, origDevice1);
-		//	return hr;
-		//}
+		// If platform_update is allowed, we have to assume that this QueryInterface off
+		// the original context will work.
+		ID3D11Device1 *origDevice1;
+		HRESULT hr = mOrigContext->QueryInterface(IID_PPV_ARGS(&origDevice1));
+		if (FAILED(hr))
+		{
+			LogInfo("  failed QueryInterface for ID3D11Device1 = %x for %p \n", hr, origDevice1);
+			LogInfo("***  return error \n");
+			return hr;
+		}
 
-		//HackerContext1 *hackerContextWrap1 = new HackerContext1(origDevice1, origDeviceContext1);
-		//LogDebug("  created HackerContext1(%s@%p) wrapper of %p \n", type_name(hackerContextWrap1), hackerContextWrap1, origDeviceContext1);
-		//HackerDevice1 *hackerDeviceWrap1 = new HackerDevice1(origDevice1, origDeviceContext1);
-		//LogDebug("  created HackerDevice1(%s@%p) wrapper of %p \n", type_name(hackerDeviceWrap1), hackerDeviceWrap1, origDevice1);
-		//hackerDeviceWrap1->SetHackerContext1(hackerContextWrap1);
-		//hackerContextWrap1->SetHackerDevice1(hackerDeviceWrap1);
+		// The original QueryInterface will have returned us the legit ID3D11DeviceContext1
+		ID3D11DeviceContext1 *origContext1 = static_cast<ID3D11DeviceContext1*>(*ppvObject);
 
-		//*ppvObject = hackerContextWrap1;
-		//LogDebug("  created HackerContext1(%s@%p) wrapper of %p \n", type_name(hackerContextWrap1), hackerContextWrap1, origDeviceContext1);
+		HackerDevice1 *hackerDeviceWrap1 = new HackerDevice1(origDevice1, origContext1);
+		LogDebug("  created HackerDevice1(%s@%p) wrapper of %p \n", type_name(hackerDeviceWrap1), hackerDeviceWrap1, origDevice1);
+
+		HackerContext1 *hackerContextWrap1 = new HackerContext1(origDevice1, origContext1);
+		LogDebug("  created HackerContext1(%s@%p) wrapper of %p \n", type_name(hackerContextWrap1), hackerContextWrap1, origContext1);
+
+		hackerDeviceWrap1->SetHackerContext1(hackerContextWrap1);
+		hackerContextWrap1->SetHackerDevice1(hackerDeviceWrap1);
+
+		*ppvObject = hackerContextWrap1;
+		LogDebug("  created HackerContext1(%s@%p) wrapper of %p \n", type_name(hackerContextWrap1), hackerContextWrap1, origContext1);
 	}
 
 	LogDebug("  returns result = %x for %p \n", hr, ppvObject);
