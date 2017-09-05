ea9b0ac49f8f630a0d86c3b17d78b3e8aaf0b98b
Ian Munsie <darkstarsword@gmail.com>
2017-09-05 15:00:32 +1000

    Software Cursor: Add linear filters
    If upscaling and the software cursor are enabled but there is either a
    mismatch between rendering the cursor before / after upscaling and
    bypassing the upscaling cursor translations, or there is some other
    unresolved issue we might get a pixelated cursor, which looks a little
    nasty. Implement a linear filter in the mouse pixel shader so that if we
    run into this situation (but things are otherwise working) the mouse
    cursor will look acceptable. This helps in Witcher3 with upscaling=1, or
    when cursor_upscaling_bypass is disabled.
    
    Because of the way we have to handle the cursor mask and colour textures
    we can't simply use a sampler to interpolate it, so this adds a very
    simple linear filter using Loads().
    
    If the cursor is being drawn 1:1 this looks identical (just as sharp) as
    before, but if the cursor is being scaled for whatever reason this now
    looks much nicer.

--- Dependencies/ShaderFixes/mouseps.hlsl
+++ Dependencies/ShaderFixes/mouseps.hlsl
@@ -6,17 +6,17 @@ Texture2D<float4> cursor_color : register(t101);
 
 // Draw a black and white, and possibly inverted cursor,
 // e.g. use "Windows Standard", "Windows Inverted" or "Windows Black" to test
-float4 draw_cursor_bw(float2 texcoord)
+float4 draw_cursor_bw(float2 texcoord, float2 dimensions)
 {
-	uint mask_width, mask_height;
 	float4 result;
 
-	cursor_mask.GetDimensions(mask_width, mask_height);
+	if (any(texcoord < 0 || texcoord >= dimensions))
+		return float4(0, 0, 0, 1);
 
 	// Black and white cursor - "the upper half is the icon AND bitmask and
 	// the lower half is the icon XOR bitmask".
 	uint xor = cursor_mask.Load(float3(texcoord, 0)).x;
-	uint and = cursor_mask.Load(float3(texcoord.x, texcoord.y + mask_height / 2, 0)).x;
+	uint and = cursor_mask.Load(float3(texcoord.x, texcoord.y + dimensions.y, 0)).x;
 
 	result.xyz = xor;
 	result.w = and ^ xor;
@@ -25,10 +25,13 @@ float4 draw_cursor_bw(float2 texcoord)
 }
 
 // Draw a colour cursor, e.g. use "Windows Default" to test
-float4 draw_cursor_color(float2 texcoord)
+float4 draw_cursor_color(float2 texcoord, float2 dimensions)
 {
 	float4 result;
 	float mask;
+
+	if (any(texcoord < 0 || texcoord >= dimensions))
+		return 0;
 
 	result = cursor_color.Load(float3(texcoord, 0));
 	mask = cursor_mask.Load(float3(texcoord, 0)).x;
@@ -58,6 +61,68 @@ float4 draw_cursor_color(float2 texcoord)
 	return result;
 }
 
+float4 smooth_cursor_bw(float2 texcoord)
+{
+	uint mask_width, mask_height;
+	float4 px1, px2, px3, px4, tmp1, tmp2;
+	float2 coord1, coord2;
+
+	cursor_mask.GetDimensions(mask_width, mask_height);
+	float2 dimensions = float2(mask_width, mask_height / 2);
+
+	// Subtracting 0.5 here to sample at the edge of each pixel instead of
+	// the center - when not scaling the cursor this will make it just as
+	// sharp as if we hadn't applied an interpolation filter at all, but
+	// still gives us a smooth result when we are scaling. Note that this
+	// can make some of the sample locations negative, which we handle in
+	// draw_cursor_bw:
+	texcoord -= 0.5;
+
+	coord1 = floor(texcoord);
+	coord2 = min(ceil(texcoord), dimensions - 1);
+
+	// Because of the special handling we have to do with the mask, we
+	// can't use a SamplerState to interpolate the cursor image, so
+	// implement a simple linear filter with loads instead:
+	px1 = draw_cursor_bw(coord1, dimensions);
+	px2 = draw_cursor_bw(float2(coord2.x, coord1.y), dimensions);
+	px3 = draw_cursor_bw(float2(coord1.x, coord2.y), dimensions);
+	px4 = draw_cursor_bw(coord2, dimensions);
+
+	tmp1 = lerp(px1, px2, frac(texcoord.x));
+	tmp2 = lerp(px3, px4, frac(texcoord.x));
+	return lerp(tmp1, tmp2, frac(texcoord.y));
+}
+
+float4 smooth_cursor_color(float2 texcoord, float2 dimensions)
+{
+	float4 px1, px2, px3, px4, tmp1, tmp2;
+	float2 coord1, coord2;
+
+	// Subtracting 0.5 here to sample at the edge of each pixel instead of
+	// the center - when not scaling the cursor this will make it just as
+	// sharp as if we hadn't applied an interpolation filter at all, but
+	// still gives us a smooth result when we are scaling. Note that this
+	// can make some of the sample locations negative, which we handle in
+	// draw_cursor_color:
+	texcoord -= 0.5;
+
+	coord1 = floor(texcoord);
+	coord2 = min(ceil(texcoord), dimensions - 1);
+
+	// Because of the special handling we have to do with the mask, we
+	// can't use a SamplerState to interpolate the cursor image, so
+	// implement a simple linear filter with loads instead:
+	px1 = draw_cursor_color(coord1, dimensions);
+	px2 = draw_cursor_color(float2(coord2.x, coord1.y), dimensions);
+	px3 = draw_cursor_color(float2(coord1.x, coord2.y), dimensions);
+	px4 = draw_cursor_color(coord2, dimensions);
+
+	tmp1 = lerp(px1, px2, frac(texcoord.x));
+	tmp2 = lerp(px3, px4, frac(texcoord.x));
+	return lerp(tmp1, tmp2, frac(texcoord.y));
+}
+
 float4 draw_cursor(float2 texcoord)
 {
 	uint color_width, color_height;
@@ -65,9 +130,9 @@ float4 draw_cursor(float2 texcoord)
 	cursor_color.GetDimensions(color_width, color_height);
 
 	if (color_width)
-		return draw_cursor_color(texcoord);
+		return smooth_cursor_color(texcoord, float2(color_width, color_height));
 	else
-		return draw_cursor_bw(texcoord);
+		return smooth_cursor_bw(texcoord);
 }
 
 void main(float4 pos : SV_Position0, float2 texcoord : TEXCOORD0, out float4 result : SV_Target0)
