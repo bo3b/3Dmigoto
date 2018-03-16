eeb55c3af6ffbe0a646dd92ff5878d8383073c0b
Ian Munsie <darkstarsword@gmail.com>
2018-03-17 06:53:07 +1100

    Blacklist QueryInterface(IDXGISwapChain4)
    Shadows of War is seen requesting this on Windows 10 shortly before a
    crash. Since we don't yet support this interface we need to blacklist
    it, however this just results in another error in Shadows of War (this
    time an actual error message about not being able to create an
    IDXGISwapChain rather than a crash).
    
    Blacklisting is still the right thing to do for now - once we switch to
    the vs2015 branch we can look at adding actual support for higher level
    DirectX objects including this one.

--- util.h
+++ util.h
@@ -797,6 +797,7 @@ struct _declspec(uuid("9d06dffa-d1e5-4d07-83a8-1bb123f2f841")) ID3D11Device2;
 struct _declspec(uuid("420d5b32-b90c-4da4-bef0-359f6a24a83a")) ID3D11DeviceContext2;
 struct _declspec(uuid("A8BE2AC4-199F-4946-B331-79599FB98DE7")) IDXGISwapChain2;
 struct _declspec(uuid("94D99BDB-F1F8-4AB0-B236-7DA0170EDAB1")) IDXGISwapChain3;
+struct _declspec(uuid("3D585D5A-BD4A-489E-B1F4-3DBCB6452FFB")) IDXGISwapChain4;
 
 std::string NameFromIID(IID id);
 
