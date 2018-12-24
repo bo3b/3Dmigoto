e01cef380b24196c419006f8806caf6181c6b15a
Ian Munsie <darkstarsword@gmail.com>
2018-12-24 14:28:11 +1100

    3DVision2SBS: Use variables instead of reserving one IniParams
    Since IniParams is not namespaced it is better that we avoid reserving
    any variables for shipped CustomShaders. This converts the 3dvision2sbs
    shader to use a global $\ShaderFixes\3dvision2sbs.ini\mode variable for
    the output mode, which it copies to an IniParams just before it runs,
    backing up the original value to a local variable first and restoring it
    afterwards.
    
    Since it no longer matters which IniParams we use, switch to x0 to
    minimise the IniParams buffer size.

--- Dependencies/ShaderFixes/3dvision2sbs.hlsl
+++ Dependencies/ShaderFixes/3dvision2sbs.hlsl
@@ -1,13 +1,14 @@
 Texture2D<float4> StereoParams : register(t125);
 Texture1D<float4> IniParams : register(t120);
 
+#define mode IniParams[0].x
+
 #ifdef VERTEX_SHADER
 void main(
 		out float4 pos : SV_Position0,
 		uint vertex : SV_VertexID)
 {
 	float4 stereo = StereoParams.Load(0);
-	float mode = IniParams.Load(int2(7, 0)).x;
 
 	// Not using vertex buffers so manufacture our own coordinates.
 	switch(vertex) {
@@ -37,7 +38,6 @@ Texture2D<float4> t100 : register(t100);
 void main(float4 pos : SV_Position0, out float4 result : SV_Target0)
 {
 	float4 stereo = StereoParams.Load(0);
-	float mode = IniParams.Load(int2(7, 0)).x;
 
 	float x = pos.x;
 	float y = pos.y;
