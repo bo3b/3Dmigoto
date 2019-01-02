771219191c87165e240cc04ee1cb617c2d258c70
Ian Munsie <darkstarsword@gmail.com>
2019-01-03 04:01:28 +1100

    d3dx.ini: Remove inadvertently uncommented 3dvision2sbs line
    Oops, not sure how that happened. Let's redo that release.

--- version.h
+++ version.h
@@ -28,7 +28,7 @@
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               3
-#define VERSION_REVISION            13
+#define VERSION_REVISION            12
  
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
