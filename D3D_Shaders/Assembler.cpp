36ff31006a5393b9be5e8a244c311941c9490060
Ian Munsie <darkstarsword@gmail.com>
2019-03-20 11:42:37 +1100

    Dis/Assembler: Handle "undecipherable custom data"
    Currently our disassembler fixup routine strips undecipherable custom
    data tags, and the assembler throws a parse error if it encounters one
    (such as, from an old shader dumped prior to the addition of the fixup
    routine).
    
    This adds support for these tags to both - when the disassembler fixup
    routine encounters one it now properly decodes the length and appends
    the raw data to the line in a hexdump fashion. When the assembler
    encounters one of these tags it will then reassemble the hexdump back
    into bytecode, and if there is no hexdump (from an old shader) it will
    skip over the instruction without assembling anything.
    
    Most of our disassemble routines will now include custom data by
    default to make sure that any tags that could cause problems are visible
    (if the shaderhacker wants to strip them out that's fine, but I'd rather
    they see the tag exists and make a conscious decision to that end), but
    for the sake of backwards compatibility with any ShaderRegex patterns
    that might be sensitive to one of these tags suddenly appearing it must
    be enabled in the d3dx.ini for ShaderRegex to include it.
    
    With this change the Project Cars example shaders are able to
    reconstruct for the HLSL tests again, and the assembly tests are
    passing.

--- D3D_Shaders/Assembler.cpp
+++ D3D_Shaders/Assembler.cpp
@@ -1427,6 +1427,28 @@ static vector<DWORD> assemble_printf(string &s, vector<DWORD> &v, vector<string>
 	return v;
 }
 
+static vector<DWORD> assemble_undecipherable_custom_data(string &s, vector<DWORD> &v, vector<string> &w)
+{
+	uint32_t numOps, word, i;
+
+	check_num_ops(s, w, 2, 0);
+	if (w[1] != "custom" || w[2] != "data")
+		throw AssemblerParseError(s, "Unrecognised instruction");
+
+	// If we only have the words "undecipherable custom data" it's from the
+	// MS disassembler and there is nothing we can really do with it, but
+	// if it went through our disassembler fixup path we appended a hexdump
+	// of the instruction that we can now reassemble:
+
+	numOps = (uint32_t)w.size() - 3;
+	for (i = 0; i < numOps; i++) {
+		sscanf_s(w[i + 3].c_str(), "%x", &word);
+		v.push_back(word);
+	}
+
+	return v;
+}
+
 static vector<DWORD> assembleIns(string s)
 {
 	unsigned msaa_samples = 0;
@@ -2224,6 +2246,8 @@ static vector<DWORD> assembleIns(string s)
 		return assemble_printf(s, v, w, false);
 	} else if (o == "errorf") {
 		return assemble_printf(s, v, w, true);
+	} else if (o == "undecipherable") {
+		return assemble_undecipherable_custom_data(s, v, w);
 	} else {
 		throw AssemblerParseError(s, "Unrecognised instruction");
 	}
@@ -2560,6 +2584,16 @@ static void hexdump_instruction(string &s, vector<DWORD> &v,
 	lines.insert(pos, hd);
 }
 
+static void encode_custom_data(string &s, vector<DWORD> &v)
+{
+	char buf[16];
+
+	for (auto val : v) {
+		_snprintf_s(buf, 16, 16, " %08x", val);
+		s += buf;
+	}
+}
+
 // This function aims to patch the RDEF comment block from d3dcompiler_47 to
 // look like the old d3dcompiler_46 format so that ShaderRegex patterns that
 // match it do not have to be updated for the newer format. There are some
@@ -2603,7 +2637,8 @@ static inline void patch_d3dcompiler_47_rdef(string *line, int *rdef_state)
 }
 
 HRESULT disassembler(vector<byte> *buffer, vector<byte> *ret, const char *comment,
-		int hexdump, bool d3dcompiler_46_compat)
+		int hexdump, bool d3dcompiler_46_compat,
+		bool disassemble_undecipherable_data)
 {
 	byte fourcc[4];
 	DWORD fHash[4];
@@ -2725,20 +2760,20 @@ HRESULT disassembler(vector<byte> *buffer, vector<byte> *ret, const char *commen
 				codeStart++;
 			}
 			if (s == "undecipherable custom data") {
-				string prev = lines[i - 1];
-				if (prev == "ret ")
-					v.clear();
-				if (v.size() == 1) {
-					// This looks inherently wrong to me - scanning for the next word with a
-					// non-zero length field, but custom data can include arbitrary data. I
-					// suspect that the below code for printf/errorf would work here as well.
-					//  -DarkStarSword
-					ins = (shader_ins*)++codeStart;
-					while (ins->length == 0) {
-						ins = (shader_ins*)++codeStart;
-					}
-				}
-				s = "";
+				// Changed this to use the instruction length
+				// in the next word like the below printf
+				// instead of scanning for a word that happened
+				// to have some bits zeroed. Removed the case
+				// to skip processing custom data immediately
+				// following a ret. -DarkStarSword
+				uint32_t len = *codeStart;
+				v.push_back(*codeStart++);
+				for (uint32_t j = 1; j < len - 1; j++)
+					v.push_back(*codeStart++);
+				if (disassemble_undecipherable_data)
+					encode_custom_data(s, v);
+				else
+					s = "";
 			} else if (v[0] == 0x00002035) {
 				// Opcode 0x35 is custom data (specifically printf/errorf).
 				// Instruction length is in the next word instead:
