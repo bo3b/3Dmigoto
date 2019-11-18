6f6d799c11f48b4e368a59b5750e0cccddae7c47
Ian Munsie <darkstarsword@gmail.com>
2019-11-18 23:01:36 +1100

    cmd_Decompiler: Use MIGOTO_DX=11 to use DX11 disassembler
    This allows the cmd_Decompiler project to compile again. Later we might
    want to add DX9 dis/assembler support to this project, but given that is
    just Microsoft's implementations of both it is not terribly interesting
    and a low priority. The DX9 decompiler will be much more interesting.

--- HLSLDecompiler/cmd_Decompiler/cmd_Decompiler.cpp
+++ HLSLDecompiler/cmd_Decompiler/cmd_Decompiler.cpp
@@ -9,6 +9,9 @@
 #include "DecompileHLSL.h"
 #include "version.h"
 #include "log.h"
+#define MIGOTO_DX 11 // Selects the DX11 disassembler in util.h - the DX9 dis/assembler is not very
+                     // interesting since it is just Microsoft's - we can add it later, but low priority.
+                     // The DX9 decompiler is more interesting, which is unrelated to this flag.
 #include "util.h"
 #include "shader.h"
 
