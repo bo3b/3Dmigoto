147cee69b39785b2646b83f4b91d15ef5c73f468
Bo3b Johnson <github@bo3b.net>
2024-08-19 01:55:23 -0700

    Update convenience copy for new paths.
    Quick sanity test in Witcher3 shows this version runs and seems OK.
    
    A couple of the other test games won't run. HeadLander hangs, and Inside is not in 3D, but does not crash and 3Dmigoto loads.

--- CopyToGames.bat
+++ CopyToGames.bat
@@ -19,12 +19,12 @@ REM xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row IV\" /F /Y /S
 REM xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Saints Row the Third\" /F /Y /S
 REM xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\F.E.A.R. 3\" /F /Y /S
 REM xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DefenseGrid2\" /F /Y /S
-xcopy /C "%1*.*" "G:\SteamLibrary\SteamApps\common\DiRT Rally\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\DiRT Rally\" /F /Y /S
 
 REM Works with hooking
-xcopy /C "%1*.*" "G:\SteamLibrary\SteamApps\common\Alien Isolation\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Alien Isolation\" /F /Y /S
 
-exit
+exit /b 0
 
 
 :Copyx64
@@ -37,16 +37,16 @@ REM xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Project CARS\" /F /Y /S
  xcopy /C "%1*.*" "W:\Games\Watch_Dogs\bin"  /F /Y /S
 
 REM Works with hooking
-xcopy /C "%1*.*" "G:\Games\INSIDE\" /F /Y /S
-xcopy /C "%1*.*" "G:\Games\The Witcher 3 Wild Hunt\bin\x64\" /F /Y /S
-xcopy /C "%1*.*" "G:\Games\Obduction\"  /F /Y /S
+xcopy /C "%1*.*" "W:\Games\INSIDE\" /F /Y /S
+xcopy /C "%1*.*" "W:\Games\The Witcher 3 Wild Hunt\bin\x64\" /F /Y /S
+xcopy /C "%1*.*" "W:\Games\Obduction\"  /F /Y /S
 
-REM xcopy /C "%1*.*" "G:\uPlay\Tom Clancy's The Division\" /F /Y /S
+REM xcopy /C "%1*.*" "W:\uPlay\Tom Clancy's The Division\" /F /Y /S
 
-xcopy /C "%1*.*" "G:\SteamLibrary\SteamApps\common\Metal Gear Solid Ground Zeroes\" /F /Y /S
-xcopy /C "%1*.*" "G:\SteamLibrary\SteamApps\common\Just Cause 3\" /F /Y /S
-xcopy /C "%1*.*" "G:\SteamLibrary\SteamApps\common\ShadowOfMordor\x64\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Metal Gear Solid Ground Zeroes\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\Just Cause 3\" /F /Y /S
+xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\ShadowOfMordor\x64\" /F /Y /S
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\HeadLander\" /F /Y /S
 xcopy /C "%1*.*" "W:\SteamLibrary\SteamApps\common\NieRAutomata\" /F /Y /S
-xcopy /C "%1*.*" "G:\Games\Batman - The Telltale Series\" /F /Y /S
+xcopy /C "%1*.*" "W:\Games\Batman - The Telltale Series\" /F /Y /S
 xcopy /C "%1*.*" "W:\SteamLibrary\steamapps\common\Dishonored2\" /F /Y /S
