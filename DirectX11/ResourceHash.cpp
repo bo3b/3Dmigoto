61cfdbdc584bd110257aa7dd14bfa3b1e65d7377
bo3b <github@bo3b.net>
2025-08-31 23:07:19 -0700

    Fix build to use new ThirdPartyLibs folder.
    Files come from the static vcpkg distribution, not locally built. We are using the static distribution to match our use of /MT through all other parts of project.
    
    Moved in a function from the source code for GetSurfaceInfo, as opposed to doing a code-drop for latest version.
    
    All 6 build targets successfully build now, but are untested.

--- DirectX11/ResourceHash.cpp
+++ DirectX11/ResourceHash.cpp
@@ -7,11 +7,24 @@
 #include "profiling.h"
 #include "overlay.h"
 
-// DirectXTK headers fail to include their own pre-requisits. We just want
-// GetSurfaceInfo from LoaderHelpers
-#include "DirectXTK/Src/pch.h"
-#include "DirectXTK/Src/PlatformHelpers.h"
-#include "DirectXTK/Src/LoaderHelpers.h"
+// DirectXTK headers failed to include their own pre-requisites.
+// We just want GetSurfaceInfo from LoaderHelpers, so that is now
+// copied directly to the file here, as it is small and self-contained.
+
+namespace DirectX
+{
+	namespace LoaderHelpers
+	{
+		HRESULT GetSurfaceInfo(
+			_In_ size_t width,
+			_In_ size_t height,
+			_In_ DXGI_FORMAT fmt,
+			_Out_opt_ size_t* outNumBytes,
+			_Out_opt_ size_t* outRowBytes,
+			_Out_opt_ size_t* outNumRows) noexcept;
+	}
+}
+
 
 // Overloaded functions to log any kind of resource description (useful to call
 // from templates):
@@ -1760,4 +1773,366 @@ bool TextureOverrideLess(const struct TextureOverride &lhs, const struct Texture
 bool FuzzyMatchResourceDescLess::operator() (const std::shared_ptr<FuzzyMatchResourceDesc> &lhs, const std::shared_ptr<FuzzyMatchResourceDesc> &rhs) const
 {
 	return TextureOverrideLess(*lhs->texture_override, *rhs->texture_override);
+}
+
+
+//--------------------------------------------------------------------------------------
+// Directly copied from DirectXTK code, as GetSurfaceInfo is not exposed in DirectXTK,
+// headers. But is simple enough to copy here.
+//--------------------------------------------------------------------------------------
+
+namespace DirectX
+{
+	namespace LoaderHelpers
+	{
+		//--------------------------------------------------------------------------------------
+		// Return the BPP for a particular format
+		//--------------------------------------------------------------------------------------
+		inline size_t BitsPerPixel(_In_ DXGI_FORMAT fmt) noexcept
+		{
+			switch (fmt)
+			{
+			case DXGI_FORMAT_R32G32B32A32_TYPELESS:
+			case DXGI_FORMAT_R32G32B32A32_FLOAT:
+			case DXGI_FORMAT_R32G32B32A32_UINT:
+			case DXGI_FORMAT_R32G32B32A32_SINT:
+				return 128;
+
+			case DXGI_FORMAT_R32G32B32_TYPELESS:
+			case DXGI_FORMAT_R32G32B32_FLOAT:
+			case DXGI_FORMAT_R32G32B32_UINT:
+			case DXGI_FORMAT_R32G32B32_SINT:
+				return 96;
+
+			case DXGI_FORMAT_R16G16B16A16_TYPELESS:
+			case DXGI_FORMAT_R16G16B16A16_FLOAT:
+			case DXGI_FORMAT_R16G16B16A16_UNORM:
+			case DXGI_FORMAT_R16G16B16A16_UINT:
+			case DXGI_FORMAT_R16G16B16A16_SNORM:
+			case DXGI_FORMAT_R16G16B16A16_SINT:
+			case DXGI_FORMAT_R32G32_TYPELESS:
+			case DXGI_FORMAT_R32G32_FLOAT:
+			case DXGI_FORMAT_R32G32_UINT:
+			case DXGI_FORMAT_R32G32_SINT:
+			case DXGI_FORMAT_R32G8X24_TYPELESS:
+			case DXGI_FORMAT_D32_FLOAT_S8X24_UINT:
+			case DXGI_FORMAT_R32_FLOAT_X8X24_TYPELESS:
+			case DXGI_FORMAT_X32_TYPELESS_G8X24_UINT:
+			case DXGI_FORMAT_Y416:
+			case DXGI_FORMAT_Y210:
+			case DXGI_FORMAT_Y216:
+				return 64;
+
+			case DXGI_FORMAT_R10G10B10A2_TYPELESS:
+			case DXGI_FORMAT_R10G10B10A2_UNORM:
+			case DXGI_FORMAT_R10G10B10A2_UINT:
+			case DXGI_FORMAT_R11G11B10_FLOAT:
+			case DXGI_FORMAT_R8G8B8A8_TYPELESS:
+			case DXGI_FORMAT_R8G8B8A8_UNORM:
+			case DXGI_FORMAT_R8G8B8A8_UNORM_SRGB:
+			case DXGI_FORMAT_R8G8B8A8_UINT:
+			case DXGI_FORMAT_R8G8B8A8_SNORM:
+			case DXGI_FORMAT_R8G8B8A8_SINT:
+			case DXGI_FORMAT_R16G16_TYPELESS:
+			case DXGI_FORMAT_R16G16_FLOAT:
+			case DXGI_FORMAT_R16G16_UNORM:
+			case DXGI_FORMAT_R16G16_UINT:
+			case DXGI_FORMAT_R16G16_SNORM:
+			case DXGI_FORMAT_R16G16_SINT:
+			case DXGI_FORMAT_R32_TYPELESS:
+			case DXGI_FORMAT_D32_FLOAT:
+			case DXGI_FORMAT_R32_FLOAT:
+			case DXGI_FORMAT_R32_UINT:
+			case DXGI_FORMAT_R32_SINT:
+			case DXGI_FORMAT_R24G8_TYPELESS:
+			case DXGI_FORMAT_D24_UNORM_S8_UINT:
+			case DXGI_FORMAT_R24_UNORM_X8_TYPELESS:
+			case DXGI_FORMAT_X24_TYPELESS_G8_UINT:
+			case DXGI_FORMAT_R9G9B9E5_SHAREDEXP:
+			case DXGI_FORMAT_R8G8_B8G8_UNORM:
+			case DXGI_FORMAT_G8R8_G8B8_UNORM:
+			case DXGI_FORMAT_B8G8R8A8_UNORM:
+			case DXGI_FORMAT_B8G8R8X8_UNORM:
+			case DXGI_FORMAT_R10G10B10_XR_BIAS_A2_UNORM:
+			case DXGI_FORMAT_B8G8R8A8_TYPELESS:
+			case DXGI_FORMAT_B8G8R8A8_UNORM_SRGB:
+			case DXGI_FORMAT_B8G8R8X8_TYPELESS:
+			case DXGI_FORMAT_B8G8R8X8_UNORM_SRGB:
+			case DXGI_FORMAT_AYUV:
+			case DXGI_FORMAT_Y410:
+			case DXGI_FORMAT_YUY2:
+#if (defined(_XBOX_ONE) && defined(_TITLE)) || defined(_GAMING_XBOX)
+			case DXGI_FORMAT_R10G10B10_7E3_A2_FLOAT:
+			case DXGI_FORMAT_R10G10B10_6E4_A2_FLOAT:
+			case DXGI_FORMAT_R10G10B10_SNORM_A2_UNORM:
+#endif
+				return 32;
+
+			case DXGI_FORMAT_P010:
+			case DXGI_FORMAT_P016:
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN10)
+			case DXGI_FORMAT_V408:
+#endif
+#if (defined(_XBOX_ONE) && defined(_TITLE)) || defined(_GAMING_XBOX)
+			case DXGI_FORMAT_D16_UNORM_S8_UINT:
+			case DXGI_FORMAT_R16_UNORM_X8_TYPELESS:
+			case DXGI_FORMAT_X16_TYPELESS_G8_UINT:
+#endif
+				return 24;
+
+			case DXGI_FORMAT_R8G8_TYPELESS:
+			case DXGI_FORMAT_R8G8_UNORM:
+			case DXGI_FORMAT_R8G8_UINT:
+			case DXGI_FORMAT_R8G8_SNORM:
+			case DXGI_FORMAT_R8G8_SINT:
+			case DXGI_FORMAT_R16_TYPELESS:
+			case DXGI_FORMAT_R16_FLOAT:
+			case DXGI_FORMAT_D16_UNORM:
+			case DXGI_FORMAT_R16_UNORM:
+			case DXGI_FORMAT_R16_UINT:
+			case DXGI_FORMAT_R16_SNORM:
+			case DXGI_FORMAT_R16_SINT:
+			case DXGI_FORMAT_B5G6R5_UNORM:
+			case DXGI_FORMAT_B5G5R5A1_UNORM:
+			case DXGI_FORMAT_A8P8:
+			case DXGI_FORMAT_B4G4R4A4_UNORM:
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN10)
+			case DXGI_FORMAT_P208:
+			case DXGI_FORMAT_V208:
+#endif
+				return 16;
+
+			case DXGI_FORMAT_NV12:
+			case DXGI_FORMAT_420_OPAQUE:
+			case DXGI_FORMAT_NV11:
+				return 12;
+
+			case DXGI_FORMAT_R8_TYPELESS:
+			case DXGI_FORMAT_R8_UNORM:
+			case DXGI_FORMAT_R8_UINT:
+			case DXGI_FORMAT_R8_SNORM:
+			case DXGI_FORMAT_R8_SINT:
+			case DXGI_FORMAT_A8_UNORM:
+			case DXGI_FORMAT_BC2_TYPELESS:
+			case DXGI_FORMAT_BC2_UNORM:
+			case DXGI_FORMAT_BC2_UNORM_SRGB:
+			case DXGI_FORMAT_BC3_TYPELESS:
+			case DXGI_FORMAT_BC3_UNORM:
+			case DXGI_FORMAT_BC3_UNORM_SRGB:
+			case DXGI_FORMAT_BC5_TYPELESS:
+			case DXGI_FORMAT_BC5_UNORM:
+			case DXGI_FORMAT_BC5_SNORM:
+			case DXGI_FORMAT_BC6H_TYPELESS:
+			case DXGI_FORMAT_BC6H_UF16:
+			case DXGI_FORMAT_BC6H_SF16:
+			case DXGI_FORMAT_BC7_TYPELESS:
+			case DXGI_FORMAT_BC7_UNORM:
+			case DXGI_FORMAT_BC7_UNORM_SRGB:
+			case DXGI_FORMAT_AI44:
+			case DXGI_FORMAT_IA44:
+			case DXGI_FORMAT_P8:
+#if (defined(_XBOX_ONE) && defined(_TITLE)) || defined(_GAMING_XBOX)
+			case DXGI_FORMAT_R4G4_UNORM:
+#endif
+				return 8;
+
+			case DXGI_FORMAT_R1_UNORM:
+				return 1;
+
+			case DXGI_FORMAT_BC1_TYPELESS:
+			case DXGI_FORMAT_BC1_UNORM:
+			case DXGI_FORMAT_BC1_UNORM_SRGB:
+			case DXGI_FORMAT_BC4_TYPELESS:
+			case DXGI_FORMAT_BC4_UNORM:
+			case DXGI_FORMAT_BC4_SNORM:
+				return 4;
+
+			case DXGI_FORMAT_UNKNOWN:
+			case DXGI_FORMAT_FORCE_UINT:
+			default:
+				return 0;
+			}
+		}
+
+		//--------------------------------------------------------------------------------------
+		// Get surface information for a particular format
+		//--------------------------------------------------------------------------------------
+		inline HRESULT GetSurfaceInfo(
+			_In_ size_t width,
+			_In_ size_t height,
+			_In_ DXGI_FORMAT fmt,
+			_Out_opt_ size_t* outNumBytes,
+			_Out_opt_ size_t* outRowBytes,
+			_Out_opt_ size_t* outNumRows) noexcept
+		{
+			uint64_t numBytes = 0;
+			uint64_t rowBytes = 0;
+			uint64_t numRows = 0;
+
+			bool bc = false;
+			bool packed = false;
+			bool planar = false;
+			size_t bpe = 0;
+			switch (fmt)
+			{
+			case DXGI_FORMAT_UNKNOWN:
+				return E_INVALIDARG;
+
+			case DXGI_FORMAT_BC1_TYPELESS:
+			case DXGI_FORMAT_BC1_UNORM:
+			case DXGI_FORMAT_BC1_UNORM_SRGB:
+			case DXGI_FORMAT_BC4_TYPELESS:
+			case DXGI_FORMAT_BC4_UNORM:
+			case DXGI_FORMAT_BC4_SNORM:
+				bc = true;
+				bpe = 8;
+				break;
+
+			case DXGI_FORMAT_BC2_TYPELESS:
+			case DXGI_FORMAT_BC2_UNORM:
+			case DXGI_FORMAT_BC2_UNORM_SRGB:
+			case DXGI_FORMAT_BC3_TYPELESS:
+			case DXGI_FORMAT_BC3_UNORM:
+			case DXGI_FORMAT_BC3_UNORM_SRGB:
+			case DXGI_FORMAT_BC5_TYPELESS:
+			case DXGI_FORMAT_BC5_UNORM:
+			case DXGI_FORMAT_BC5_SNORM:
+			case DXGI_FORMAT_BC6H_TYPELESS:
+			case DXGI_FORMAT_BC6H_UF16:
+			case DXGI_FORMAT_BC6H_SF16:
+			case DXGI_FORMAT_BC7_TYPELESS:
+			case DXGI_FORMAT_BC7_UNORM:
+			case DXGI_FORMAT_BC7_UNORM_SRGB:
+				bc = true;
+				bpe = 16;
+				break;
+
+			case DXGI_FORMAT_R8G8_B8G8_UNORM:
+			case DXGI_FORMAT_G8R8_G8B8_UNORM:
+			case DXGI_FORMAT_YUY2:
+				packed = true;
+				bpe = 4;
+				break;
+
+			case DXGI_FORMAT_Y210:
+			case DXGI_FORMAT_Y216:
+				packed = true;
+				bpe = 8;
+				break;
+
+			case DXGI_FORMAT_NV12:
+			case DXGI_FORMAT_420_OPAQUE:
+				if ((height % 2) != 0)
+				{
+					// Requires a height alignment of 2.
+					return E_INVALIDARG;
+				}
+				planar = true;
+				bpe = 2;
+				break;
+
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN10)
+
+			case DXGI_FORMAT_P208:
+				planar = true;
+				bpe = 2;
+				break;
+
+#endif
+
+			case DXGI_FORMAT_P010:
+			case DXGI_FORMAT_P016:
+				if ((height % 2) != 0)
+				{
+					// Requires a height alignment of 2.
+					return E_INVALIDARG;
+				}
+				planar = true;
+				bpe = 4;
+				break;
+
+#if (defined(_XBOX_ONE) && defined(_TITLE)) || defined(_GAMING_XBOX)
+
+			case DXGI_FORMAT_D16_UNORM_S8_UINT:
+			case DXGI_FORMAT_R16_UNORM_X8_TYPELESS:
+			case DXGI_FORMAT_X16_TYPELESS_G8_UINT:
+				planar = true;
+				bpe = 4;
+				break;
+
+#endif
+
+			default:
+				break;
+			}
+
+			if (bc)
+			{
+				uint64_t numBlocksWide = 0;
+				if (width > 0)
+				{
+					numBlocksWide = std::max<uint64_t>(1u, (uint64_t(width) + 3u) / 4u);
+				}
+				uint64_t numBlocksHigh = 0;
+				if (height > 0)
+				{
+					numBlocksHigh = std::max<uint64_t>(1u, (uint64_t(height) + 3u) / 4u);
+				}
+				rowBytes = numBlocksWide * bpe;
+				numRows = numBlocksHigh;
+				numBytes = rowBytes * numBlocksHigh;
+			}
+			else if (packed)
+			{
+				rowBytes = ((uint64_t(width) + 1u) >> 1) * bpe;
+				numRows = uint64_t(height);
+				numBytes = rowBytes * height;
+			}
+			else if (fmt == DXGI_FORMAT_NV11)
+			{
+				rowBytes = ((uint64_t(width) + 3u) >> 2) * 4u;
+				numRows = uint64_t(height) * 2u; // Direct3D makes this simplifying assumption, although it is larger than the 4:1:1 data
+				numBytes = rowBytes * numRows;
+			}
+			else if (planar)
+			{
+				rowBytes = ((uint64_t(width) + 1u) >> 1) * bpe;
+				numBytes = (rowBytes * uint64_t(height)) + ((rowBytes * uint64_t(height) + 1u) >> 1);
+				numRows = height + ((uint64_t(height) + 1u) >> 1);
+			}
+			else
+			{
+				const size_t bpp = BitsPerPixel(fmt);
+				if (!bpp)
+					return E_INVALIDARG;
+
+				rowBytes = (uint64_t(width) * bpp + 7u) / 8u; // round up to nearest byte
+				numRows = uint64_t(height);
+				numBytes = rowBytes * height;
+			}
+
+#if defined(_M_IX86) || defined(_M_ARM) || defined(_M_HYBRID_X86_ARM64)
+			static_assert(sizeof(size_t) == 4, "Not a 32-bit platform!");
+			if (numBytes > UINT32_MAX || rowBytes > UINT32_MAX || numRows > UINT32_MAX)
+				return HRESULT_FROM_WIN32(ERROR_ARITHMETIC_OVERFLOW);
+#else
+			static_assert(sizeof(size_t) == 8, "Not a 64-bit platform!");
+#endif
+
+			if (outNumBytes)
+			{
+				*outNumBytes = static_cast<size_t>(numBytes);
+			}
+			if (outRowBytes)
+			{
+				*outRowBytes = static_cast<size_t>(rowBytes);
+			}
+			if (outNumRows)
+			{
+				*outNumRows = static_cast<size_t>(numRows);
+			}
+
+			return S_OK;
+		}
+	}
 }
