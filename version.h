0d147228ca9d969f2b25334cedfa80f201459b33
CI Build <3Dmigoto@bo3b.net>
2025-09-04 09:16:37 +0000

    CI Build - 1.4.4

--- version.h
+++ version.h
@@ -29,7 +29,7 @@
 
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               4
-#define VERSION_REVISION            3
+#define VERSION_REVISION            4
  
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
